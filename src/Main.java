fe6220b721d41d0c4caab01dab0ce68ed3877b9a
Greg Schaper <98821244+gregschaper@users.noreply.github.com>
2025-02-11 09:24:04 -0500

    Update Main.java
    add Main.java to src folder

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,12 @@
-
+public class Main
+  {
+    public static void main(String[] args)
+    {
+      System.out.println("Greg Schaper");
+      System.out.println("Another Schaper");
+      System.out.println("TESTER");
+      System.out.println("TESTER");
+      System.out.println("TESTER");
+      
+    }
+  }
