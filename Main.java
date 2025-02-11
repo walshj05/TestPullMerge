76e1260fd3bc6a3e2b162503bf92b5dcceb05de3
Greg Schaper <98821244+gregschaper@users.noreply.github.com>
2025-02-11 08:45:44 -0500

    Update Main.java
    Fix typo

--- Main.java
+++ Main.java
@@ -7,6 +7,6 @@ public class Main
       System.out.println("TESTER");
       System.out.println("TESTER");
       System.out.println("TESTER");
-      System.out.println("Bruce Schaper");
+      
     }
   }
