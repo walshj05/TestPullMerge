a10d2179bd6d871fdd429095990a5020a93ec999
Sydney Shifman <shifmans@moravian.edu>
2025-02-11 17:55:01 -0500

    a

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,6 @@ public class Main
       System.out.println("TESTER");
       System.out.println("Jon Walsh");
       System.out.println("Sean Creveling");
+      System.out.println("Sydney Shifman");
     }
   }
