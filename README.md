f9eaa613e630121a8606e476a076aa8ae071151e
walshj05 <walshj05@moravian.edu>
2025-02-12 14:51:30 -0500

    Deleted other fake names

--- README.md
+++ README.md
@@ -1,9 +1,5 @@
 # TestPullMerge
 Test the merging of local code when pulling from a modified repository
-* John Smith
-* Jane Doe
-* Greg Schaper
-* Another Developer
 * Jon Walsh
 * Sean Creveling
 * Sydney Shifman
--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,6 @@ public class Main
   {
     public static void main(String[] args)
     {
-      System.out.println("Greg Schaper");
-      System.out.println("Another Schaper");
-      System.out.println("TESTER");
-      System.out.println("TESTER");
-      System.out.println("TESTER");
       System.out.println("Jon Walsh");
       System.out.println("Sean Creveling");
       System.out.println("Sydney Shifman");
